4ec6f7d2663a6bc7a548be254f56deaadaddb536
arsalanans47 <arsalanans8377@gmail.com>
2026-03-31 21:40:03 +0530

    added error files

---
+++ src/utils/errors/app-error.js
@@ -0,0 +1,20 @@
+
+
+
+class AppError extends Error {
+  constructor(
+    name,
+    message,
+    explanation,
+    statusCodes
+  ){
+    super();
+    this.name = name;
+    this.message = message;
+    this.explanation = explanation;
+    this.statusCodes = statusCodes;
+  }
+}
+
+
+module.exports = AppError;
---
+++ src/utils/errors/validation-error.js
@@ -0,0 +1,20 @@
+const { StatusCodes } = require('http-status-codes');
+
+class validationError extends Error {
+  constructor(error){
+
+    super();
+
+    let explanation = [];
+    error.errors.forEach((err) => {
+      explanation.push(err.message);
+    });
+    this.name = 'validationError';
+    this.message = 'Not able to validate the data sent in the request';
+    this.explanation = explanation;
+    this.statusCodes = StatusCodes.BAD_REQUEST;
+  }
+}
+
+
+module.exports = validationError;
